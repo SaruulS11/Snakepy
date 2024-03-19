197d3768c65ef7b38e4d8c2aad5c33980a4ec726
saruul <saruul1255@gmail.com>
2024-03-19 11:58:23 +0800

    easymode

--- Easysnake.py
+++ Easysnake.py
@@ -159,11 +159,12 @@ while True:
 		fruit_position[0], fruit_position[1], 10, 10))
 
 	# Game Over conditions
-	if snake_position[0] < 0 or snake_position[0] > window_x-10:
-		game_over()
+	if snake_position[0] < 0:
+		snake_position[0] = window_x
+
 	if snake_position[1] < 0 or snake_position[1] > window_y-10:
 		game_over()
-
+  
 	# Touching the snake body
 	for block in snake_body[1:]:
 		if snake_position[0] == block[0] and snake_position[1] == block[1]:
